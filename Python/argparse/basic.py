7b2f7da695617d1c80cb20bb2a1ca2b9fada33a1
OTex911 <otex911@gmail.com>
2019-03-11 04:23:04 -0400

    fix the value when argument is not specified

--- Python/argparse/basic.py
+++ Python/argparse/basic.py
@@ -3,7 +3,7 @@
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("square", type=int, help="display a square of a given number")
-parser.add_argument("-v", "--verbosity",action="count", help="increase output verbosity")
+parser.add_argument("-v", "--verbosity",action="count", help="increase output verbosity",default=0)
 args = parser.parse_args()
 answer = args.square ** 2
 if args.verbosity >= 2:
