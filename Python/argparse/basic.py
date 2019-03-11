3417651434d163be47f9bfeb34cbb2bd27b0d652
code <code@gmail.com>
2019-03-11 04:00:34 -0400

    add short argument

--- Python/argparse/basic.py
+++ Python/argparse/basic.py
@@ -2,7 +2,7 @@
 
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument("--verbose", help="increase output verbosity", action="store_true")
+parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
 args = parser.parse_args()
 if args.verbose:
     print("verbosity turned on")
