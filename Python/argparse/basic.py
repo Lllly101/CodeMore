95ed4bd4c223c846bac183a2424438ad6d50f366
code <code@gmail.com>
2019-03-11 02:00:43 -0400

    fix an error

--- Python/argparse/basic.py
+++ Python/argparse/basic.py
@@ -4,4 +4,4 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('square', help="display a square of a given number", type=int)
 args = parser.parse_args()
-print(args.echo**2)
+print(args.square**2)
