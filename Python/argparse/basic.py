5b40a2edc231d20f651b55c51ecccfb0c354296f
code <code@gmail.com>
2019-03-11 01:59:26 -0400

    tell the argparse the type

--- Python/argparse/basic.py
+++ Python/argparse/basic.py
@@ -2,6 +2,6 @@
 
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument('square', help="display a square of a given number")
+parser.add_argument('square', help="display a square of a given number", type=int)
 args = parser.parse_args()
 print(args.echo**2)
