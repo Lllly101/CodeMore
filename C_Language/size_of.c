73b39b8aa441df7d963c93a51cc1a63c39a37daa
code <otex911@gmail.com>
2019-07-12 00:23:46 +0800

    add: long double size

--- C_Language/size_of.c
+++ C_Language/size_of.c
@@ -6,7 +6,7 @@ int a;
 char b;
 
 /*abbrev of long int c*/ 
-long c; 
+long  double c; 
 
 printf("int size: %d\n", sizeof(int));
 printf("short size: %d\n", sizeof(short));
@@ -14,6 +14,7 @@ printf("long size: %d\n", sizeof(long));
 printf("char size: %d\n", sizeof(char));
 printf("float size: %d\n", sizeof(float));
 printf("double size: %d\n", sizeof(double));
+printf("long double size: %d\n", sizeof(c));
 
 return 0;
 }
