f47b7f3637f9874621385241726c505e79ad3ccc
code <otex911@gmail.com>
2019-07-12 00:37:23 +0800

    comment the error

--- C_Language/size_of.c
+++ C_Language/size_of.c
@@ -7,7 +7,9 @@ char b;
 
 /*abbrev of long int c*/ 
 long  double c; 
-long char test;
+// long char test;
+
+signed char a;
 
 printf("int size: %d\n", sizeof(int));
 printf("short size: %d\n", sizeof(short));
@@ -15,8 +17,7 @@ printf("long size: %d\n", sizeof(long));
 printf("char size: %d\n", sizeof(char));
 printf("float size: %d\n", sizeof(float));
 printf("double size: %d\n", sizeof(double));
-printf("long double size: %d\n", sizeof(c));
-printf("long char size: %d\n", sizeof(test));
+// printf("long double size: %d\n", sizeof(c));
 
 return 0;
 }
