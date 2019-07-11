aa530dfc188d6b76d75cf0d0f5f21fe84aac2adb
code <otex911@gmail.com>
2019-07-12 00:30:12 +0800

    add: define long char error

--- C_Language/size_of.c
+++ C_Language/size_of.c
@@ -7,6 +7,7 @@ char b;
 
 /*abbrev of long int c*/ 
 long  double c; 
+long char test;
 
 printf("int size: %d\n", sizeof(int));
 printf("short size: %d\n", sizeof(short));
@@ -15,6 +16,7 @@ printf("char size: %d\n", sizeof(char));
 printf("float size: %d\n", sizeof(float));
 printf("double size: %d\n", sizeof(double));
 printf("long double size: %d\n", sizeof(c));
+printf("long char size: %d\n", sizeof(test));
 
 return 0;
 }
