57c14567d7d2d034d6038a1c626f5228570c17d9
code <otex911@gmail.com>
2019-07-12 00:22:02 +0800

    update: macos c type size

--- C_Language/size_of.c
+++ C_Language/size_of.c
@@ -8,10 +8,12 @@ char b;
 /*abbrev of long int c*/ 
 long c; 
 
-printf("%d\n", sizeof(a));
-printf("%d\n", sizeof(8));
-printf("%d\n", sizeof(b));
-printf("%d\n", sizeof(int));
+printf("int size: %d\n", sizeof(int));
+printf("short size: %d\n", sizeof(short));
+printf("long size: %d\n", sizeof(long));
+printf("char size: %d\n", sizeof(char));
+printf("float size: %d\n", sizeof(float));
+printf("double size: %d\n", sizeof(double));
 
 return 0;
 }
