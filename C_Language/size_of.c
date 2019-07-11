5665c299bea24b7782cdffa0191f209208923de8
code <otex911@gmail.com>
2019-07-11 17:04:12 +0800

    close #2

--- C_Language/size_of.c
+++ C_Language/size_of.c
@@ -5,9 +5,9 @@ int main( ) {
 int a;
 char b;
 
-printf("%ld\n", sizeof(a));
-printf("%ld\n", sizeof(8));
-printf("%ld\n", sizeof(b));
+printf("%d\n", sizeof(a));
+printf("%d\n", sizeof(8));
+printf("%d\n", sizeof(b));
 
 return 0;
 }
