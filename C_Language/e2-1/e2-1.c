1c9c82b80d1fadf649e2df1425b121b86f8eb4ec
code <otex911@gmail.com>
2019-07-12 16:53:28 +0800

    bug: long time executed

--- C_Language/e2-1/e2-1.c
+++ C_Language/e2-1/e2-1.c
@@ -14,11 +14,18 @@ signed long int g=0;
 a = 1;
 while ( a > 0) {
     a++;
-    }
-
+}
 printf("sizeof int is: %ld\n", sizeof(a));
 printf("int最小值: %d\n", a);
 printf("int最大值： %d\n", a-1);
+
+b = 1;
+while ( b > 0 ) {
+    b++;
+}
+printf("sizeof float is %ld\n", sizeof(b));
+printf("float最大值: %f\n", b);
+printf("float最小值: %f\n", b-1);
 
 return 0;
 }
