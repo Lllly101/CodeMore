89322804b0e393892c60f71069be0b6239db6dfc
code <otex911@gmail.com>
2019-07-14 23:24:45 +0800

    feat: exercies

---
+++ C_Language/Chapter2/2-9.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main() {
+
+int a, b;
+printf("Please enter the first number:");
+scanf("%d", &a);
+printf("Enter the second:");
+scanf("%d", &b);
+
+printf("\n");
+printf("a + b = %d\n", a+b);
+printf("a - b = %d\n", a-b);
+printf("a * b = %d\n", a*b);
+printf("a / b = %d\n", a/b);
+
+return 0;
+
+}
